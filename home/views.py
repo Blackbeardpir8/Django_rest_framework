f24205326cfc15ac72ef4be96c03e72e01f91566
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2025-02-17 11:15:05 +0530

    basic api view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view()
+def index(request):
+    data = {
+        "status" : True,
+        "message" : "This is from django rest framework"
+    }
+    return Response(data)
--- restframework/urls.py
+++ restframework/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home.views import index
 urlpatterns = [
+    path('api/',index, ),
     path('admin/', admin.site.urls),
 ]
