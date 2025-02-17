74891d9550d91a20b2e9151154b261bb6ceea52b
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2025-02-17 11:35:02 +0530

    which method are we using

--- home/views.py
+++ home/views.py
@@ -5,8 +5,13 @@ from rest_framework.response import Response
 
 @api_view()
 def index(request):
+    print(request.method)
+    students = ["Deepak" , "Rahul" , "Hemlata" , "Girish"]
     data = {
         "status" : True,
-        "message" : "This is from django rest framework"
+        "message" : "This is from django rest framework",
+        "students" : students,
+        "method" : f"Yor are getting this data from {request.method}"
+
     }
     return Response(data)
