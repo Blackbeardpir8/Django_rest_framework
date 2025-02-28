f539e4fefe612a66e3a64014f0f2678ee2aa0670
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2025-02-28 15:54:07 +0530

    login

--- home/serializers.py
+++ home/serializers.py
@@ -44,3 +44,7 @@ class RegisterSerializer(serializers.Serializer):
         )
     
         return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length = 100)
+    password = serializers.CharField(max_length = 100)
--- home/views.py
+++ home/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from home.models import Student,Book,Product
-from home.serializers import StudentSerializer,BookSerializer,ProductSerializer,RegisterSerializer
+from home.serializers import StudentSerializer,BookSerializer,ProductSerializer,RegisterSerializer, LoginSerializer
 from rest_framework.views import APIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin
 from rest_framework.generics import GenericAPIView
 from rest_framework import generics
 from rest_framework import viewsets 
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 # Create your views here.
 
 class StudentModelListView(ListModelMixin,CreateModelMixin,GenericAPIView):
@@ -224,3 +227,29 @@ class RegisterAPI(APIView):
             "message": "Invalid data",
             "data": serializer.errors
         })
+    
+
+class LoginAPI(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data = request.data)
+        if serializer.is_valid():
+            user = authenticate(
+                username = serializer.data['username'],
+                password = serializer.data['password']
+            )
+
+            if user is None:
+                return Response({
+                    "status": False,
+                    "message": "Invalid Credantials",
+                    "data" : {}
+                })
+            
+            token,created = Token.objects.get_or_create(user = user)
+            return Response({
+                    "status": False,
+                    "message": "user token",
+                    "data" : {
+                        "token":token.key
+                        }
+                })
